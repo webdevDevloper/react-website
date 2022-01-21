ab74f1630609feb53267f617fc72ecde0a044dff
Nguyen Kiet <nguyenkiet0807@gmail.com>
2022-01-21 16:29:21 +0700

    style trang gio hang oke

--- src/pages/ShoppingCart/ItemsCard.jsx
+++ src/pages/ShoppingCart/ItemsCard.jsx
@@ -1,31 +1,58 @@
 import InputNumber from "components/InputNumber/InputNumber";
 import React from "react";
+import testImage from "assets/22.jpg";
+import { Link } from "react-router-dom";
+import { ImCross } from "react-icons/im";
 
 function ItemCard(props) {
 	return (
-		<div className="mb-6 border">
-			<div className="flex justify-between px-4 py-2">X</div>
-			<hr />
-			<div className="flex justify-between px-4 py-2">
-				<div className="font-semibold">Product name</div>
-				<div>Ten san pham</div>
+		<>
+			<div className="mb-6 border md:hidden">
+				<div className="flex justify-between px-4 py-2 cursor-pointer">
+					<ImCross style={{ color: "#dc2626" }} />
+				</div>
+				<hr />
+				<div className="flex justify-between px-4 py-2">
+					<div className="font-semibold">Product name</div>
+					<div>Ten san pham</div>
+				</div>
+				<hr />
+				<div className="flex justify-between px-4 py-2">
+					<div className="font-semibold">Price</div>
+					<div>Ten san pham</div>
+				</div>
+				<hr />
+				<div className="flex justify-between px-4 py-2 items-center">
+					<div className="font-semibold">Quantity</div>
+					<InputNumber className="w-24 h-8" height="h-8" />
+				</div>
+				<hr />
+				<div className="flex justify-between px-4 py-2">
+					<div className="font-semibold">Subtotal</div>
+					<div>100</div>
+				</div>
 			</div>
-			<hr />
-			<div className="flex justify-between px-4 py-2">
-				<div className="font-semibold">Price</div>
-				<div>Ten san pham</div>
+			<div className="hidden md:grid grid-cols-8 mb-4 text-xl items-center gap-4 px-4">
+				<div className="cursor-pointer">
+					<ImCross style={{ color: "#dc2626" }} />
+				</div>
+				<img
+					src={testImage}
+					className="w-32 h-24 object-contain justify-self-center"
+					alt=""
+				/>
+				<Link to={`#`} className="col-span-3 justify-self-center">
+					{" "}
+					Ten san pham asd asd s asd asd asd asd
+				</Link>
+				<div className="justify-self-center">1000</div>
+				<div className="justify-self-center">
+					<InputNumber />
+				</div>
+				<div className="justify-self-center">1000</div>
 			</div>
-			<hr />
-			<div className="flex justify-between px-4 py-2 items-center">
-				<div className="font-semibold">Quantity</div>
-				<InputNumber className="w-24 h-8" height="h-8" />
-			</div>
-			<hr />
-			<div className="flex justify-between px-4 py-2">
-				<div className="font-semibold">Subtotal</div>
-				<div>100</div>
-			</div>
-		</div>
+			<hr className="hidden md:block mb-4" />
+		</>
 	);
 }
 
--- src/pages/ShoppingCart/ShoppingCart.jsx
+++ src/pages/ShoppingCart/ShoppingCart.jsx
@@ -1,16 +1,53 @@
-import InputNumber from "components/InputNumber/InputNumber";
+import PrimaryButton, { SecondButton } from "components/StyledButton/Button";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import ItemCard from "./ItemsCard";
 
 const ShoppingCart = () => {
+	let navigate = useNavigate();
+	const handleKeepShopping = () => {
+		navigate("/");
+	};
+
 	return (
-		<div className="flex flex-col mx-auto items-center">
-			<div>Your cart</div>
-			<div className="w-10/12">
-				<ItemCard />
-				<ItemCard />
+		<div className="flex flex-col mx-auto items-center w-10/12 md:w-11/12 text-gray-800 md:flex-row">
+			<div className="w-full md:w-9/12 md:mr-4">
+				<div className="text-3xl mb-4 font-semibold text-center">
+					Your cart
+				</div>
+				<div className="w-full">
+					<ItemCard />
+					<ItemCard />
+				</div>
 			</div>
-			<div>asd</div>
+
+			<div className="w-full md:w-1/4">
+				<div className="text-3xl mb-4 font-semibold text-center">
+					Purchase Information
+				</div>
+				<div className="w-full border  text-xl mb-4">
+					<div className="flex justify-between px-4 py-2">
+						<div className="font-semibold">Total Quantity</div>
+						<div>2</div>
+					</div>
+					<hr />
+					<div className="flex justify-between px-4 py-2">
+						<div className="font-semibold">Total</div>
+						<div>$10</div>
+					</div>
+					<hr />
+				</div>
+
+				<PrimaryButton className="mb-4 shadow-md md:text-xl">
+					Proceed to checkout
+				</PrimaryButton>
+				<SecondButton
+					onClick={handleKeepShopping}
+					className="mb-4 shadow-md md:text-xl"
+				>
+					Keep shopping
+				</SecondButton>
+			</div>
 		</div>
 	);
 };
